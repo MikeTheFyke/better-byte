49372b1daba4cda602fe802c897fb8b5883448e4
MikeTheFyke <mikefyke@hotmail.com>
2024-04-12 10:43:19 -0400

    Adding recipe images and details to recipe cards

--- app/(components)/RecipeCard.tsx
+++ app/(components)/RecipeCard.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unescaped-entities */
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Recipe } from "../(models)/Recipe";
 import AddLoblaws from "./AddLoblaws";
 import AddMetro from "./AddMetro";
@@ -5,6 +7,7 @@ import AddNoFrills from "./AddNoFrills";
 import AddWalmart from "./AddWalmart";
 import DeleteCard from "./DeleteCard";
 import EditCard from "./EditCard";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 interface Props {
 	recipe: Recipe;
@@ -14,18 +17,35 @@ const RecipeCard = ({ recipe }: Props) => {
 	console.log("Recipes incoming : ", recipe);
 	return (
 		<div className="w-[400px] h-[400px] bg-slate-100 hover:bg-slate-300 rounded-xl">
-			<div className="flex justify-end p-1">
+			<div className="flex justify-between p-1">
 				<DeleteCard />
+				<div className="flex text-black place-items-center justify-center text-2xl">
+					{recipe.title}
+				</div>
 				<EditCard />
 			</div>
-			<div className="bg-red-200 h-[260px] grid grid-cols-2 gap-4 place-content-between">
+			<div
+				className="h-[260px] grid grid-cols-2 gap-4 place-content-between"
+				style={{
+					backgroundImage: `url(${recipe.image.file})`,
+					backgroundRepeat: "no-repeat",
+					backgroundSize: "cover",
+					backgroundPosition: "center",
+				}}
+			>
 				<AddWalmart />
 				<AddLoblaws />
 				<AddMetro />
 				<AddNoFrills />
 			</div>
-			<div className="flex text-black text-center justify-center text-2xl pt-5">
-				{recipe.title}
+			<div className="flex text-slate-500 italic place-items-center justify-center text-2xl pt-4 h-[50px]">
+				"{recipe.description}"
+			</div>
+			<div className="flex justify-end text-white text-2xl p-1">
+				<div className="w-[60px] h-[30px] bg-blue-500 flex place-items-center justify-center rounded-tl-[10px] rounded-br-[10px]">
+					<FontAwesomeIcon icon={faUser} className="text-xl mr-2" />
+					{recipe.servings.toString()}
+				</div>
 			</div>
 		</div>
 	);
