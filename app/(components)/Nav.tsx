d512701dd13cf2cad746c78fee1b7af82970fd35
MikeTheFyke <mikefyke@hotmail.com>
2024-04-13 22:38:43 -0400

    Github and Google auth flow working

--- app/(components)/Nav.tsx
+++ app/(components)/Nav.tsx
@@ -7,8 +7,12 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import SearchBar from "./SearchBar";
+import { getServerSession } from "next-auth";
+import { options } from "../api/auth/[...nextauth]/options";
 
-const Nav = () => {
+const Nav = async () => {
+	const session = await getServerSession(options);
+
 	return (
 		<nav className="flex justify-between bg-nav p-4">
 			<div className="flex items-center space-x-10">
@@ -33,12 +37,36 @@ const Nav = () => {
 					<p className="text-default-text">Logout</p>
 				</div> */}
 				<div className="mt-2 flex justify-between space-x-4">
-					<button
-						type="submit"
-						className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-					>
-						Login
-					</button>
+					{session ? (
+						<Link
+							href="/api/auth/signout?callbackUrl=/"
+							style={{ display: "contents" }}
+						>
+							<button
+								type="button"
+								className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+							>
+								Logout
+							</button>
+						</Link>
+					) : (
+						<>
+							<Link href="/api/auth/signin" style={{ display: "contents" }}>
+								<button
+									type="button"
+									className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+								>
+									Login
+								</button>
+							</Link>
+							<button
+								type="submit"
+								className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+							>
+								Register
+							</button>
+						</>
+					)}
 				</div>
 			</div>
 		</nav>
