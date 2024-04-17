7e8f23c124165fbc55a6b7d1cc05257cd0afe478
MikeTheFyke <mikefyke@hotmail.com>
2024-04-17 13:31:36 -0400

    Building ingredient grid

--- app/RecipePage/[user_id]/[id]/page.tsx
+++ app/RecipePage/[user_id]/[id]/page.tsx
@@ -1,3 +1,4 @@
+import IngredientGrid from "@/app/(components)/IngredientGrid/IngredientGrid";
 import RecipeCard from "@/app/(components)/RecipeCard";
 import { Recipe, getRecipeById } from "@/app/(models)/Recipe";
 
@@ -13,7 +14,7 @@ const RecipePage = async ({ params }: any) => {
 					<RecipeCard recipe={selectedRecipe} userId={params.user_id} />
 				) : null}
 			</div> */}
-			<div className="flex flex-col">
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4">
 				<div
 					className="h-[260px]"
 					style={{
@@ -22,28 +23,38 @@ const RecipePage = async ({ params }: any) => {
 						backgroundSize: "cover",
 						backgroundPosition: "center",
 					}}
-				></div>
-				<h6>{selectedRecipe.title}</h6>
-				<h6>{selectedRecipe.description}</h6>
-				<h6>Servings: {selectedRecipe.servings.toString()}</h6>
-				<h1>Ingredients</h1>
-				{selectedRecipe.ingredients.map((ingredient) => {
-					return (
-						<>
-							<li>
-								{ingredient.name} {ingredient.quantity} {ingredient.unit}
-							</li>
-						</>
-					);
-				})}
-				<h1>Steps</h1>
-				{selectedRecipe.steps.map((step) => {
-					return (
-						<>
-							<li>{step}</li>
-						</>
-					);
-				})}
+				/>
+				<div>
+					<h6 className="text-4xl">{selectedRecipe.title}</h6>
+					<h6>{selectedRecipe.description}</h6>
+					<h6>Serves: {selectedRecipe.servings.toString()}</h6>
+				</div>
+				<div>
+					<div className="mb-4">
+						<h1 className="text-2xl mb-2">Ingredients</h1>
+						{selectedRecipe.ingredients.map((ingredient) => {
+							return (
+								<div className="flex justify-between" key={ingredient.name}>
+									<h6 className="w-[200px]">
+										{ingredient.quantity} {ingredient.unit}
+									</h6>
+									<h6 className="w-[200px]">{ingredient.name}</h6>
+								</div>
+							);
+						})}
+					</div>
+					<IngredientGrid />
+					<div>
+						<h1 className="text-2xl  mb-2">Steps</h1>
+						{selectedRecipe.steps.map((step) => {
+							return (
+								<>
+									<li>{step}</li>
+								</>
+							);
+						})}
+					</div>
+				</div>
 			</div>
 		</div>
 	);
